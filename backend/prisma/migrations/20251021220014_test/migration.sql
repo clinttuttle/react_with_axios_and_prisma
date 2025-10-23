cb9c1b86cf032b77fd128f2e6f7ca0940568597a
Clint Tuttle <tuttlejc@eid.utexas.edu>
2025-10-23 13:12:38 -0500

    first commit

---
+++ backend/prisma/migrations/20251021220014_test/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE `employees` (
+    `employee_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `first_name` VARCHAR(50) NULL,
+    `last_name` VARCHAR(50) NULL,
+    `email` VARCHAR(50) NULL,
+    `birthdate` DATE NULL,
+    `salary` INTEGER NULL,
+
+    PRIMARY KEY (`employee_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
